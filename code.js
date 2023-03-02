69be619c92735c884e3f4225041c94bc6f20edd0
Nya0813 <125600175+Nya0813@users.noreply.github.com>
2023-03-02 16:54:50 -0300

    Add files via upload

--- code.js
+++ code.js
@@ -5,7 +5,7 @@ window.preload = function () {
 
   p5Inst._predefinedSpriteAnimations = {};
   p5Inst._pauseSpriteAnimationsByDefault = false;
-  var animationListJSON = {"orderedKeys":["d2b4284f-fe64-4b4a-8d87-81b58834f72b","77ddc667-9e85-4b05-8d2c-3ef0fb98c933","b16c59a8-bb88-4d36-bc8d-5305672d8707","875d8528-b0a7-499e-a4db-dd5a6e6debce","942b3258-e5df-4c29-bb48-60cc01fc9d69"],"propsByKey":{"d2b4284f-fe64-4b4a-8d87-81b58834f72b":{"name":"quadra","sourceUrl":"assets/api/v1/animation-library/gamelab/4cK4OL0TYp17v4XpNg9ICEQ_SgSA7nCh/category_backgrounds/sports_basketballhalf2.png","frameSize":{"x":400,"y":400},"frameCount":1,"looping":true,"frameDelay":2,"version":"4cK4OL0TYp17v4XpNg9ICEQ_SgSA7nCh","categories":["backgrounds"],"loadedFromSource":true,"saved":true,"sourceSize":{"x":400,"y":400},"rootRelativePath":"assets/api/v1/animation-library/gamelab/4cK4OL0TYp17v4XpNg9ICEQ_SgSA7nCh/category_backgrounds/sports_basketballhalf2.png"},"77ddc667-9e85-4b05-8d2c-3ef0fb98c933":{"name":"aluno","sourceUrl":"assets/api/v1/animation-library/gamelab/ZU9n84i5bHGhnk4Ft2idQyV9REOHhz0I/category_people/blue_shirt2.png","frameSize":{"x":136,"y":399},"frameCount":1,"looping":true,"frameDelay":2,"version":"ZU9n84i5bHGhnk4Ft2idQyV9REOHhz0I","categories":["people"],"loadedFromSource":true,"saved":true,"sourceSize":{"x":136,"y":399},"rootRelativePath":"assets/api/v1/animation-library/gamelab/ZU9n84i5bHGhnk4Ft2idQyV9REOHhz0I/category_people/blue_shirt2.png"},"b16c59a8-bb88-4d36-bc8d-5305672d8707":{"name":"basketball","sourceUrl":"assets/api/v1/animation-library/gamelab/lJ_EH4DV2ueKL_rNgl9vTVZREP_YfLJf/category_sports/basketball.png","frameSize":{"x":393,"y":394},"frameCount":1,"looping":true,"frameDelay":2,"version":"lJ_EH4DV2ueKL_rNgl9vTVZREP_YfLJf","categories":["sports"],"loadedFromSource":true,"saved":true,"sourceSize":{"x":393,"y":394},"rootRelativePath":"assets/api/v1/animation-library/gamelab/lJ_EH4DV2ueKL_rNgl9vTVZREP_YfLJf/category_sports/basketball.png"},"875d8528-b0a7-499e-a4db-dd5a6e6debce":{"name":"soccerball","sourceUrl":"assets/api/v1/animation-library/gamelab/KAKckB.0WJDP55kNGzIZIfW5wf7Rk5mG/category_sports/soccer_bw.png","frameSize":{"x":393,"y":394},"frameCount":1,"looping":true,"frameDelay":2,"version":"KAKckB.0WJDP55kNGzIZIfW5wf7Rk5mG","categories":["sports"],"loadedFromSource":true,"saved":true,"sourceSize":{"x":393,"y":394},"rootRelativePath":"assets/api/v1/animation-library/gamelab/KAKckB.0WJDP55kNGzIZIfW5wf7Rk5mG/category_sports/soccer_bw.png"},"942b3258-e5df-4c29-bb48-60cc01fc9d69":{"name":"volleyball","sourceUrl":"assets/api/v1/animation-library/gamelab/JTd581LwNfOIZ0FzKo.ais_jFYPyV4_G/category_sports/volleyball.png","frameSize":{"x":393,"y":394},"frameCount":1,"looping":true,"frameDelay":2,"version":"JTd581LwNfOIZ0FzKo.ais_jFYPyV4_G","categories":["sports"],"loadedFromSource":true,"saved":true,"sourceSize":{"x":393,"y":394},"rootRelativePath":"assets/api/v1/animation-library/gamelab/JTd581LwNfOIZ0FzKo.ais_jFYPyV4_G/category_sports/volleyball.png"}}};
+  var animationListJSON = {"orderedKeys":[],"propsByKey":{}};
   var orderedKeys = animationListJSON.orderedKeys;
   var allAnimationsSingleFrame = false;
   orderedKeys.forEach(function (key) {
@@ -34,72 +34,160 @@ window.preload = function () {
     }
 // -----
 
-var quadra = createSprite(200,200);
-quadra.setAnimation("quadra");
+var playerMallet;
 
-var aluno = createSprite(200,350);
-aluno.setAnimation("aluno");
-aluno.scale =.2;
+var goal1=createSprite(200,18,100,20);
+goal1.shapeColor=("red");
 
-var basketball = createSprite(20,130);
-basketball.setAnimation("basketball");
-basketball.scale =.1;
-
-var soccerball = createSprite(380,200);
-soccerball.setAnimation("soccerball");
-soccerball.scale =.1;
-
-var volleyball = createSprite(20,250);
-volleyball.setAnimation("volleyball")
-volleyball.scale =.1
-
-var trave = createSprite(200,0,100,20);
-trave.shapeColor = "white"
-
-basketball.setVelocity(8,0);
-soccerball.setVelocity(-6,0);
-volleyball.setVelocity(8,0);
+var goal2=createSprite(200,382,100,20);
+goal2.shapeColor=("yellow");
 
 
-createEdgeSprites();
+//criando quadra
+var boundary1 = createSprite(200,0,400,10);
+boundary1.shapeColor = "white";
+var boundary2 = createSprite(200,400,400,10);
+boundary2.shapeColor = "white";
+var boundary3 = createSprite(0,200,10,400);
+boundary3.shapeColor = "white";
+var boundary4 = createSprite(400,200,10,400);
+boundary4.shapeColor = "white";
+
+
+
+// criando objetos e lhes dando cores
+var striker = createSprite(200,200,10,10);
+striker.shapeColor = "white";
+
+var playerMallet = createSprite(200,50,50,10);
+playerMallet.shapeColor = "black";
+
+var computerMallet = createSprite(200,350,50,10);
+computerMallet.shapeColor = "black";
+
+//variáveis de pontuação
+var playerScore=0;
+var compScore=0;
 
 function draw() {
+  //limpar a tela
+  background("green");
   
+  //exibir pontuação
+  textSize(18);
+  fill("maroon");
+  text("0" +playerScore, 25,225);
+  text("0"+compScore,25,185);
+  
+  // Pontuação
+  
+     if(striker.isTouching(goal2))
+      { //aumente a pontuação do jogador
+        compScore = compScore+1 ;
+        //use show grid para identificar o valor de x e y para trazer o atacante para o centro
+        striker.x=5;
+        striker.y=5;
+        striker.velocityX=5;
+        striker.velocityY=5;
+      }
+      
+      if(striker.isTouching(goal1))
+      {
+        playerScore = playerScore + 1;
+        //Redefina o atacante adicionando o valor central de x e y
+        striker.x=5;
+        striker.y=5;
+        striker.velocityX=5;
+        striker.velocityY=5;
+      }
+      
+   
+      if(compScore===5) //adicione a condição para verificar se a pontuação do jogador chegou a 5
 
+      {
+        fill("maroon");
+        textSize(18);
+        //adicione o texto para fim de jogo
+        text(" Gamer over ",170,160);
+      }
  
+        if(playerScore===5) //adicione a condição para verificar se a pontuação do jogador chegou a 5
 
-basketball.bounceOff(edges);
-soccerball.bounceOff(edges);
-volleyball.bounceOff(edges);
-aluno.bounceOff(edges);
+      {
+        fill("maroon");
+        textSize(18);
+        //adicione o texto para fim de jogo
+        text(" Gamer over ",170,160);
+      }
+   
+      
  
- 
-if (keyDown("w")) {
-   aluno.y = aluno.y -4;
- }
-  if (keyDown("s")) {
-   aluno.y = aluno.y +4;
- }
- if (keyDown("d")) {
-   aluno.x = aluno.x +4;
- }
- if (keyDown("a")) {
-   aluno.x = aluno.x -4;
- }
- 
- if (aluno.isTouching(basketball) || aluno.isTouching(soccerball) || aluno.isTouching(volleyball)) {
-  aluno.x = 200
-  aluno.y = 350
-  playSound("assets/category_sports/One_Whistle_SFX.mp3")
+  
+  //faça o bastão do jogador se mover com as teclas de seta
+  paddleMovement();
+  
+  
+  //inteligência artificial para o bastão do computador
+  //faça com que se mova com a posição y do atacante
+  computerMallet.x = striker.x;
 
- }
- if (aluno.isTouching(trave)) {
-   playSound("assets/category_alerts/playful_quirky_positive_game_cue_1.mp3")
- }
- 
+  
+  //desenhe uma linha no centro
+   for (var i = 0; i < 400; i=i+20) {
+    line(i,200,i+10,200);
+  }
+  
+  //crie bordas de limite
+  //faça com que o atacante rebata nas bordas de cima e de baixo
+  createEdgeSprites();
+  
+  striker.bounceOff(edges);
+  striker.bounceOff(playerMallet);
+  striker.bounceOff(computerMallet);
+  playerMallet.bounceOff(edges);
+  computerMallet.bounceOff(edges);
 
+  
+  //lance o atacante quando a tecla espaço for pressionada
+  if (keyDown("space")) {
+    serve();
+  }
+  
+ 
   drawSprites();
 }
+function serve() {
+  striker.velocityX = 10;
+  striker.velocityY = 5;
+ 
+}
+
+function paddleMovement()
+{
+  if(keyDown("left")){
+    playerMallet.x = playerMallet.x-10;
+    
+  }
+  
+  if(keyDown("right")){
+    playerMallet.x = playerMallet.x+10;
+    
+  }
+  
+  if(keyDown("up")){
+   if(playerMallet.y>25)
+   {
+    playerMallet.y = playerMallet.y- 10;
+   }
+  }
+  
+  if(keyDown("down")){
+    if(playerMallet.y<120)
+   {
+    playerMallet.y = playerMallet.y+10;
+   }
+  }
+} 
 
 // -----
     try { window.draw = draw; } catch (e) {}
